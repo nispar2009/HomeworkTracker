cbe6008d0a8c73acaa7dfbfaa2a829416877aed6
nispar2009 <varun.parupalli@gmail.com>
2022-04-13 12:05:57 +0530

    v3.0

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 let hw, counter
 
 hw = []
-doing = 0
 counter = 1
 
 update = () => {
-    done = 0
     total = 0
     for (const item of hw) {
         if (item.compl == true) {
@@ -17,35 +15,20 @@ update = () => {
 
     setValue("hw", "")
 
-    if (done == total - 1) {
+    if (total == 1) {
         setValue("motivate", "Just one to go!")
     }
-    else if (done == total) {
+    else if (total == 0) {
         setValue("motivate", "No work remaining!")
     }
     else {
-        setValue("motivate", "")
+        setValue("motivate", "Full speed ahead!")
     }
 
     for (const hwItem of hw) {
-        let btnCompl, p
-
-        if (hwItem.compl) {
-            btnCompl = "<button class='btn btn-green-outline btn-sm' onclick='markCompl(" + hwItem.id + ")'>Mark as unfinished</button>"
-            p = "<p><del>" + hwItem.subject + ": " + hwItem.cnt + "</del><p>"
-        }
-        else {
-            btnCompl = "<button class='btn btn-green-outline btn-sm' onclick='markCompl(" + hwItem.id + ")'>Mark as finished</button>"
-            p = "<p>" + hwItem.subject + ": " + hwItem.cnt + "<p>"
-        }
-
-        if (doing == hwItem.id) {
-            p = "<p>" + hwItem.subject + ": " + hwItem.cnt + " (doing)<p>"
-        }
-
+        let p = hwItem.cnt
         let btnDel = "<button class='btn btn-red-outline btn-sm' onclick='delHw(" + hwItem.id + ")'>Delete</button>"
-        let btnDo = "<button class='btn btn-blue-outline btn-sm' onclick='setDoing(" + hwItem.id + ")'>Mark as doing</button>"
-        document.getElementById("hw").innerHTML += "<li>" + p + btnCompl + " " + btnDo + " " + btnDel + "</li>"
+        document.getElementById("hw").innerHTML += "<li>" + p + btnDel + "</li>"
     }
 
     console.log(hw)
@@ -78,40 +61,5 @@ delHw = id => {
 
     hw = updatedHw
 
-    update()
-}
-
-markCompl = id2 => {
-    hw = hw.map(item => {
-        if (item.id == id2) {
-            if (item.compl) {
-                return ({
-                    id: item.id,
-                    subject: item.subject,
-                    cnt: item.cnt,
-                    compl: false
-                })
-            }
-            else {
-                return ({
-                    id: item.id,
-                    subject: item.subject,
-                    cnt: item.cnt,
-                    compl: true
-                })
-            }
-        }
-        else {
-            return(item)
-        }
-    })
-
-    doing = 0
-
-    update()
-}
-
-setDoing = hwItem => {
-    doing = hwItem
     update()
 }
